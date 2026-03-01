c88e06fa776f163b635e5ec12d65720b04570345
Liriali4 <lirialia51@gmail.com>
2026-03-01 09:33:33 +0100

    Feat: User entity and resource

---
+++ src/main/java/com/webservice/vendas/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.webservice.vendas.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.webservice.vendas.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Líria", "lia@gmail.com", "9999", "1234");
+		return ResponseEntity.ok().body(u);
+
+	}
+}
